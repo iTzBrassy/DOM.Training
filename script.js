5f5f8a4617ab3b95a2d5f946a36482d5e670f263
iTzBrassy <77305703+iTzBrassy@users.noreply.github.com>
2022-06-06 18:02:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -13,35 +13,69 @@
 //     console.log("Click!");
 // });
 
+// Instantiating elements //
 let button = document.getElementById("enter");
 let input = document.getElementById("userinput");
 let ul = document.querySelector("ul");
+let items = document.querySelectorAll("li");
 
-// function that gets the input length
+// function: gets the input length //
 let intputLength = () => {
   return input.value.length;
 };
 
-// function for creating the list element
+// function: creating the new list items //
 let createListElement = () => {
   // creates element
   let li = document.createElement("li");
+
   // creates the input value text insilde the li
   li.appendChild(document.createTextNode(input.value));
+
+  //new button for delete
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+  deleteButton.addEventListener("click", function () {
+    li.parentNode.removeChild(li);
+  });
   // adds the li to the end of the ul
+  li.appendChild(deleteButton);
   ul.appendChild(li);
   input.value = "";
 };
+// function: toggles the done class/style //
 
-// function for button click
+// Existing Items: edit //
+Array.from(items).forEach(function (item) {
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+  item.appendChild(deleteButton);
+  deleteButton.addEventListener("click", function () {
+    item.parentNode.removeChild(item);
+  });
+
+  //   item.classList.toggle("done");
+  item.addEventListener("click", function () {
+    item.classList.toggle("done");
+  });
+});
+
+// function: removes listed item //
+let removeListedItem = () => {
+  items.parentElement.removeChild(items);
+};
+
+// APPLYING FUNCTIONS //-----------------------------------------------------
+
+// function: button "Enter" click
 let addListAfterClick = () => {
   if (intputLength() > 0) {
     createListElement();
   }
-}; 
+};
 
-// function for enter press
-//"e" = event
+// function: "Enter" Keypress
+// "e" = event
 
 let addListAfterEnterPress = (e) => {
   if (intputLength() > 0 && e.code === "Enter") {
@@ -49,8 +83,12 @@ let addListAfterEnterPress = (e) => {
   }
 };
 
+let deleteItem = () => {
+  removeButton();
+  removeListedItem();
+};
+
 // on button click
 button.addEventListener("click", addListAfterClick);
-
 // on enter press
 input.addEventListener("keypress", addListAfterEnterPress);
